df1a1c1c1094aaf07eab8c73ae5403b0f3a87a02
Andy Fragen <andy@thefragens.com>
2016-12-06 12:03:20 -0800

    bump tested to

--- CHANGES.md
+++ CHANGES.md
@@ -1,3 +1,6 @@
+####  1.0.3
+* bump tested to
+
 #### 1.0.2
 * tested to 4.3
 
--- the-events-calendar-outlook-import-fix.php
+++ the-events-calendar-outlook-import-fix.php
@@ -1,16 +1,16 @@
 <?php
-/*
-Plugin Name:       The Events Calendar Outlook Import Fix
-Plugin URI:        https://github.com/afragen/the-events-calendar-outlook-import-fix
-Description:       This plugin fixes the import of calendar events from <a href="http://wordpress.org/plugins/the-events-calendar/">The Events Calendar</a> to Outlook.
-Version:           1.0.3
-Author:            Andy Fragen
-Author URI:        http://wp.me/p3OvSb-kF
-License:           GNU General Public License v2
-License URI:       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-GitHub Plugin URI: https://github.com/afragen/the-events-calendar-outlook-import-fix
-GitHub Branch:     develop
-*/
+/**
+ * Plugin Name:       The Events Calendar Outlook Import Fix
+ * Plugin URI:        https://github.com/afragen/the-events-calendar-outlook-import-fix
+ * Description:       This plugin fixes the import of calendar events from <a href="http://wordpress.org/plugins/the-events-calendar/">The Events Calendar</a> to Outlook.
+ * Version:           1.0.3
+ * Author:            Andy Fragen
+ * Author URI:        http://wp.me/p3OvSb-kF
+ * License:           GNU General Public License v2
+ * License URI:       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * GitHub Plugin URI: https://github.com/afragen/the-events-calendar-outlook-import-fix
+ * GitHub Branch:     develop
+ */
 
 add_filter( 'tribe_ical_properties', 'tribe_ical_outlook_modify', 10, 2 );
 
